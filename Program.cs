83940dc536a82e1219da3ca424f6e1a4319d8895
poojakhandale <poojakhandale2000@gmail.com>
2023-02-25 21:04:11 +0530

    Peak and pop from the Stack

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static StackandQueue.MyStack;
+
+namespace StackandQueue
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Mystack<int> stack = new Mystack<int>();
+            stack.Push(70);
+            stack.Push(37);
+            stack.Push(56);
+        }
+
+    }
+}

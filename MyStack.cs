28c2e205cf6066dd79014a49e2afa51e6129e97c
poojakhandale <poojakhandale2000@gmail.com>
2023-02-25 20:53:10 +0530

    Create a stack

---
+++ MyStack.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackandQueue
+{
+    public class MyStack
+    {
+        public class Mystack<T>
+        {
+            private LinkedList<T> list = new LinkedList<T>();
+
+            public void Push(T num)
+            {
+                list.AddFirst(num);
+            }
+
+        }
+
+    }
+}
